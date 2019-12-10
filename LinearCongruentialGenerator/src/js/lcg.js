730ff9093de6fda44eb1c1de23739ba53eb735c1
Oliver Merkel <Merkel.Oliver@web.de>
2019-12-10 14:37:31 +0100

    Update lcg.js

--- LinearCongruentialGenerator/src/js/lcg.js
+++ LinearCongruentialGenerator/src/js/lcg.js
@@ -13,10 +13,13 @@
  * Section 8.5: double-precision 64-bit format IEEE 754 values plus special NaN
  * The mantisse is build on 53-bit
  *
+ * This implementation is MINSTD_rand0. Thus the LCG s = ( a*r +c ) % m simplifies using c=0.
+ * Algorithm is "Schrage's method" sometimes referred as "approximate factoriation mathod.
+ * 
  * References:
- * Payne, Rabung, Bogyo, "Coding the Lehmer pseudo-random number generator", Communications of the ACM Volume 12 #2: p.85–86, 1969.
- * Stephen K. Park and Keith W. Miller, "Random Number Generators: Good Ones Are Hard To Find", Communications of the ACM Volume 31 #10: p.1192–1201, 1988.
- *
+ * Payne, Rabung, Bogyo, "Coding the Lehmer pseudo-random number generator", Communications of the ACM Volume 12 #2: p.85â€“86, 1969.
+ * Stephen K. Park and Keith W. Miller, "Random Number Generators: Good Ones Are Hard To Find", Communications of the ACM Volume 31 #10: p.1192â€“1201, 1988.
+ * Linus Schrage, "A More Portable Fortran Random Number Generator", ACM transactions on Mathematical Software, Vol.5, No. 2: p.132-138, June 1979.
  */
 
 function LinearCongurentialGenerator( a, m, q, r ) {
@@ -38,4 +41,3 @@ LinearCongurentialGenerator.prototype.getInt = function() {
   this.seed = xn < 0 ? xn + this.m : xn;
   return this.seed;
 }
-
